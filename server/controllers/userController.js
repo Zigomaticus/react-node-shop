9b69b170b9a90fbb3889f71e28ad2dc3cd76cea7
denis <permwed@gmail.com>
2022-02-19 10:29:59 +0500

    start work

---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+const ApiError = require("../error/ApiError");
+
+class userController {
+  async registration(req, res) {}
+
+  async login(req, res) {}
+
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest('Not found ID'));
+    }
+    res.json(id);
+  }
+}
+
+module.exports = new userController();
