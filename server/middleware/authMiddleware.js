d2a2ae4db1d4955bd9cc5005eeab6559c53b892d
denis <permwed@gmail.com>
2022-02-22 12:42:28 +0500

    End back

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -7,12 +7,12 @@ module.exports = function (req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      res.status(401).json({ message: "User not authorization" });
+      return res.status(401).json({ message: "User not auth" });
     }
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
     next();
-  } catch (err) {
-    res.status(401).json({ message: "User not authorization" });
+  } catch (e) {
+    res.status(401).json({ message: "User not auth" });
   }
 };
