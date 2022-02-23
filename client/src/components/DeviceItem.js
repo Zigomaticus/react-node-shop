40460c2fcbe162abfeeb21163eafe7b06e1415cd
denis <permwed@gmail.com>
2022-02-23 19:36:20 +0500

    create device page

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { Col, Card, Image } from "react-bootstrap";
 import star from "../assets/star.svg";
+import { useNavigate } from "react-router-dom";
+import { DEVICE_ROUTE } from "../utils/consts";
 
 const DeviceItem = ({ device }) => {
+  const navigate = useNavigate();
+  console.log(navigate);
+
   return (
-    <Col md={3}>
+    <Col
+      md={3}
+      className="mt-3"
+      onClick={() => navigate(DEVICE_ROUTE + "/" + device.id)}
+    >
       <Card style={{ width: 150, cursor: "pointer" }} border={"light"}>
         <Image width={150} height={150} src={device.img} />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+        <div className="d-flex justify-content-between align-items-center mt-1 text-black-50">
           <div>Samsumg...</div>
-          <div>
+          <div className="d-flex align-items-center">
             <div>{device.raiting}</div>
-            <Image src={star} />
+            <Image src={star} width={18} height={18} />
           </div>
         </div>
+        <div>{device.name}</div>
       </Card>
     </Col>
   );
